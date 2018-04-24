2b138ad7ae5a6ba79fcc24e44a109a144b193c55
Brian Baso <brianbaso@gmail.com>
2018-04-24 15:35:43 -0400

    react notes

---
+++ react-examples/clicker.js
@@ -0,0 +1,16 @@
+var clicker = React.createClass({ 
+	getInitialState: function () {
+	 return { count: 0};
+	  },
+	  handleClick: function() {
+	  	this.setState({ count: this.state.count + 1});
+	  },
+	  render: function() {
+	  	return React.DOM.a(
+	  	{onClick: this.handleClick},
+	  	'You have clicked ' + this.state.count + ' times'
+	  	);
+	  }
+	});
+
+React.renderComponent(Clicker(), mountNode);
